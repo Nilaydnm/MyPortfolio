5c9429e483c2ca2072c3a829e55293e243b70cc8
Nilay <nilaydonmez390@gmail.com>
2025-10-07 18:25:53 +0300

    Add EF Core setup and entity models for MyPortfolio
    Added Entity Framework Core dependencies to `MyPortfolio.csproj` for database access, migrations, and tooling. Created `MyPortfolioContext` class to configure SQL Server connection and define `DbSet` properties for entities. Added entity classes (`About`, `Contact`, `Experience`, `Feature`, `Message`, `Portfolio`, `Skill`, `SocialMedia`, and `Testimonial`) in the `MyPortfolio.DAL.Entities` namespace to represent database tables.

---
+++ MyPortfolio/DAL/Entities/Experience.cs
@@ -0,0 +1,13 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Experience
+    {
+        public int ExperienceId { get; set; }
+        public string Head { get; set; }
+
+        public string Title { get; set; }
+
+        public string Date { get; set; }
+        //metin tabanlı olduğu için string olarak tanımladım
+    }
+}
